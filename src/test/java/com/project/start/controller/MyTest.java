7c85675591ff882c3cd1c1f48ac1f96cad7c2b52
Mirzayev <orkhan.mirzayev@accenture.com>
2017-08-06 21:33:39 +0300

    update project

--- src/test/java/com/project/start/controller/MyTest.java
+++ src/test/java/com/project/start/controller/MyTest.java
@@ -1,21 +1,47 @@
 package com.project.start.controller;
 
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
+import com.project.start.SeleniumExampleApplication;
 
-public class MyTest{
+@SpringBootTest(classes = {SeleniumExampleApplication.class})
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= {SeleniumExampleApplication.class})
+@WebIntegrationTest("server.port:0")    
+
+public class MyTest {
+
+	
+	  @Autowired
+	    EmbeddedWebApplicationContext server;
+
+	    @Value("${local.server.port}")
+	    int port;    
 
 	@Test
 	public void test() {
-		HtmlUnitDriver driver = new HtmlUnitDriver();
+//		System.out.println("Environment port: ===>>>> "+environment.getProperty("local.server.port"));
+		HtmlUnitDriver driver = new HtmlUnitDriver(true);
 		driver.get("http://localhost:8089/test");
 		WebDriverWait wait = new WebDriverWait(driver, 3000);
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("username"))));
@@ -33,6 +59,7 @@ public class MyTest{
 		loginButton.click();
 		System.out.println("===================================================================================================================");
 		driver.quit();
+//		System.out.println("Server port: "+randomServerPort);
 		assertEquals("1", "1");
 	}
 
